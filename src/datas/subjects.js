4e8cd56f67e27971cefe107442f978ecc92fc13d
PedroDourado352 <phdourado4@gmail.com>
2026-05-30 10:11:22 -0300

    Update

---
+++ src/datas/subjects.js
@@ -0,0 +1,9 @@
+export const SUBJECTS = [
+  { id: "mat", name: "Matemática", color: "#1F4234" },
+  { id: "red", name: "Redação", color: "#D98A3D" },
+  { id: "fis", name: "Física", color: "#C4623D" },
+  { id: "his", name: "História", color: "#5B7A8C" },
+  { id: "bio", name: "Biologia", color: "#5E8C6A" },
+  { id: "ing", name: "Inglês", color: "#7E6AA6" }
+];
+
